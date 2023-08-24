6cbf983b07d8b9ff19d1f1ec511895d84fb9494f
colejacks7 <cjackson33@huskers.unl.edu>
2023-08-24 10:16:06 -0500

    initial commit

---
+++ hello3.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Cole Jackson
+ * Date: 2023/08/24
+ *
+ * saying my name in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Cole Jackson\n");
+
+  return 0;
+}
